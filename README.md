9b2438e0c90098575dea5cfe220fa856e049c4d2
SigneAnnaMaria <signe.annamaria@gmail.com>
2023-11-10 15:42:57 +0100

    tracking users currently in a room and users who have visited a room to send messages to them as well. delete empty room

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# chatapp-backend
+# Chat App Backend
+
+The backend for a simple real-time chat application created with `socket.io` and
+`express` as part of a school project.
+
+## Quick Start
+
+1. Clone the repo:
+   `git clone https://github.com/signeannamaria/chatapp-backend.git`
+2. Install dependencies: `npm install`
+3. Start the server: `node server.js`
+4. The frontend is required as well:
+   `https://github.com/signeannamaria/chatapp.git`
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const http = require('http')
 const socketIo = require('socket.io')
-const cors = require('cors')
 
 const app = express()
 const server = http.createServer(app)
@@ -14,97 +13,135 @@ const io = socketIo(server, {
 })
 
 let rooms = ['Lobby']
+const usersInRooms = {}
 const roomMessages = { Lobby: [] }
-const roomsUsersMap = {}
+const userRoomVisitation = {}
 const socketUserMap = {}
 
 io.on('connection', (socket) => {
   socket.emit('rooms_list', rooms)
-  console.log('users in lobby')
-  console.log(roomsUsersMap)
 
   socket.on('get_rooms_list', () => {
     socket.emit('rooms_list', rooms)
   })
 
-  socket.on('create_room', (roomName) => {
+  socket.on('create_room', (roomName, callback) => {
     if (!rooms.includes(roomName)) {
       rooms.push(roomName)
       roomMessages[roomName] = []
       io.emit('rooms_list', rooms)
+      callback({ roomCreated: true, roomName: roomName })
+    } else {
+      callback({ roomCreated: false })
     }
   })
 
   socket.on('join_room', (data) => {
-    console.log('data')
-    console.log(data)
-    console.log(
-      'server registered a user: ' +
-        data.username +
-        'joining a room: ' +
-        data.roomId
-    )
+    const { roomId, username } = data
+    userRoomVisitation[username] = userRoomVisitation[username] || new Set()
+    userRoomVisitation[username].add(roomId)
+    socketUserMap[socket.id] = username
 
-    console.log('users in room when joining a new room')
-    console.log(roomsUsersMap)
-    const roomId = data.roomId
-    const username = data.username
-
-    if (!roomsUsersMap[roomId]) {
-      roomsUsersMap[roomId] = []
+    if (!usersInRooms[roomId]) {
+      usersInRooms[roomId] = new Set()
     }
-    roomsUsersMap[roomId].push(username)
-
-    console.log('roomsUsersMap[roomId]')
-    console.log(roomsUsersMap[roomId])
+    usersInRooms[roomId].add(username)
 
     socket.join(roomId)
-    io.to(roomId).emit('users_list', roomsUsersMap[roomId])
-    io.emit('test_event', 'This is a test message.')
+    io.to(roomId).emit('users_list', Array.from(usersInRooms[roomId]))
   })
 
   socket.on('leave_room', (data) => {
     const { roomId, username } = data
-    if (roomsUsersMap[roomId]) {
-      roomsUsersMap[roomId] = roomsUsersMap[roomId].filter(
-        (user) => user !== username
-      )
-      socket.leave(roomId)
-      io.to(roomId).emit('users_list', roomsUsersMap[roomId])
+
+    if (usersInRooms[roomId] && usersInRooms[roomId].has(username)) {
+      usersInRooms[roomId].delete(username)
+
+      if (usersInRooms[roomId].size === 0) {
+        if (roomId !== 'Lobby') {
+          deleteRoom(roomId)
+        } else {
+          delete usersInRooms[roomId]
+        }
+      } else {
+        io.to(roomId).emit('users_list', Array.from(usersInRooms[roomId]))
+      }
+    }
+
+    if (userRoomVisitation[username]) {
+      userRoomVisitation[username].delete(roomId)
+      if (userRoomVisitation[username].size === 0) {
+        delete userRoomVisitation[username]
+      }
     }
   })
 
   socket.on('send_message', (data) => {
-    console.log('data.room')
-    console.log(data.room)
-    const room = data.room
-    const messageContent = {
-      username: data.username,
-      text: data.message,
-      room: data.room,
-    }
+    const { room, username, message } = data
+    const messageContent = { username, text: message, room }
+
     if (!roomMessages[room]) {
       roomMessages[room] = []
     }
     roomMessages[room].push(messageContent)
-    console.log('roomMessages')
-    console.log(roomMessages)
+
     io.to(room).emit('receive_message', messageContent)
+
+    Object.entries(userRoomVisitation).forEach(([user, visitedRooms]) => {
+      if (visitedRooms.has(room) && user !== username) {
+        const userSockets = Object.entries(socketUserMap)
+          .filter(([_, userOfSocket]) => userOfSocket === user)
+          .map(([socketId]) => socketId)
+
+        userSockets.forEach((socketId) => {
+          const currentRooms = Array.from(
+            io.sockets.sockets.get(socketId)?.rooms || []
+          )
+          if (!currentRooms.includes(room)) {
+            io.to(socketId).emit('receive_message', messageContent)
+          }
+        })
+      }
+    })
   })
 
   socket.on('disconnect', () => {
     const username = socketUserMap[socket.id]
-    console.log('diconnected username: ' + username)
-    delete socketUserMap[socket.id]
-    for (const roomId in roomsUsersMap) {
-      roomsUsersMap[roomId] = roomsUsersMap[roomId].filter(
-        (user) => user !== username
-      )
-      io.to(roomId).emit('users_list', roomsUsersMap[roomId])
+    if (username) {
+      Object.keys(usersInRooms).forEach((roomId) => {
+        if (usersInRooms[roomId].has(username)) {
+          usersInRooms[roomId].delete(username)
+          if (roomId !== 'Lobby' && usersInRooms[roomId].size === 0) {
+            deleteRoom(roomId)
+          } else {
+            io.to(roomId).emit('users_list', Array.from(usersInRooms[roomId]))
+          }
+        }
+      })
+
+      if (userRoomVisitation[username]) {
+        userRoomVisitation[username].forEach((roomId) => {
+          userRoomVisitation[username].delete(roomId)
+        })
+        delete userRoomVisitation[username]
+      }
+
+      delete socketUserMap[socket.id]
     }
   })
 })
 
+function deleteRoom(roomId) {
+  rooms = rooms.filter((room) => room !== roomId)
+  delete roomMessages[roomId]
+  io.emit('rooms_list', rooms)
+  io.emit('delete-room', roomId)
+}
+
+function getRoomUsers(roomId) {
+  return usersInRooms[roomId] ? Array.from(usersInRooms[roomId]) : []
+}
+
 server.listen(3001, () => {
-  console.log('Server listening on port 3001')
+  console.log('Server is running and listening on port 3001')
 })
